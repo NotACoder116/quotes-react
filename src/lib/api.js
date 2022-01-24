1d7255aa5ded375e2bd7394b74a058cea2157ef0
Yadwinder Singh <yadwinder96@gmail.com>
2022-01-24 21:00:30 +0530

    firebase url changed

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = 'https://react-quotes-ef4f2-default-rtdb.firebaseio.com';
+const FIREBASE_DOMAIN = 'https://react-quotes-1-55b96-default-rtdb.firebaseio.com';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
